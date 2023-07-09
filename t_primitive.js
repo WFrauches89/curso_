4c1c47d99b296bd52f83127192ce7487964c9ead
WFrauches89 <101157962+WFrauches89@users.noreply.github.com>
2023-07-09 20:15:12 -0300

    exemplos aula 01 - JS

---
+++ t_primitive.js
@@ -0,0 +1,55 @@
+// Tipo primitivos de Number
+
+        const meuNumero = 3;
+
+        const firstNumber = 1;
+        const secondNumber = 2;
+
+        const operMath = firstNumber + secondNumber;
+        console.log(operMath);
+
+        // ponto flutuantes 
+
+        const floatNumber = 3.3;
+        const floatNumber2 = .5;
+
+        // NaN - Not a Number 
+
+        const alura = "Alura";
+
+        console.log(alura * floatNumber);
+
+// Tipo primitivos booleanos
+const booleA = 5;
+const booleB = 10;
+const booleC = "10";
+
+console.log(booleA === booleB);
+console.log(booleB === booleC);
+console.log(booleB == booleC);
+
+
+
+//Tipo primitivo String
+//Todos os itens diferentes de numeros e booleanos - entre aspas duplas ou simples
+
+const stringA = "Hello world!";
+const stringB = "123";
+const stringCitacao = 'E então ele disse: "Como aconteceu isso?"';
+
+console.log(stringA + stringB + stringCitacao ); //concatenacao
+
+
+
+//ordenação de array de números no JavaScript 
+//lista.sort((a, b) => a - b); // [1, 5, 8, 9, 10, 12, 15]
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
