a0c4edfdfb5d105a7c2eda815bd3d088cbafb06c
WFrauches89 <101157962+WFrauches89@users.noreply.github.com>
2023-07-25 13:35:53 -0300

    "Ajustes on stage"

--- Algorit/aula01/cheaper.js
+++ Algorit/aula01/cheaper.js
@@ -3,7 +3,6 @@ const books = require("./bookslist.js");
 
 function priceCurrent (listPos, positionActual) {
     let cheaper = positionActual;
-    // let expensive = positionActual;
 
      for (let i = positionActual; i < listPos.length; i++){
          if(listPos[i].price < listPos[cheaper].price){
@@ -11,18 +10,13 @@ function priceCurrent (listPos, positionActual) {
         }
     
     }
-    // for (let i = positionActual; i < listPos.length; i++){
-    //     if(listPos[i].price > listPos[expensive].price){
-    //         expensive = i;
-    //     }
-    // }
+
     return cheaper;
-    // return expensive;
+
 }
+
+let smaller = priceCurrent(books, 0);
 
 module.exports = priceCurrent;
        
-// console.log(`O livro mais barato custa R$ ${books[smaller].price},00 e o nome do livro é ${books[smaller].title}.`);
-
-// console.log(`O livro mais caro custa R$${books[taller].price},00 e o nome do livro é ${books[taller].title}.`);
-
+console.log(`O livro mais barato custa R$ ${books[smaller].price},00 e o nome do livro é ${books[smaller].title}.`);
